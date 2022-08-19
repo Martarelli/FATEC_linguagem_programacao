f1d1ab04aa31f64e92dbad53cabfe968a598fd12
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-08-19 19:42:48 -0300

    primeiro exemplo de operação ternária

--- Aula 2/expressao_condicional.py
+++ Aula 2/expressao_condicional.py
@@ -1,1 +1,7 @@
 #CRIE UM PROGRAMA QUE CALCULE A MEDIA DE DUAS PROVAS E APRESENTE SE O ALUNO ESTÁ APROVADO OU REPROVADO
+p1 = int(input("Digite a nota da p1: "))
+p2 = int(input("Digite a nota da p2: "))
+media = (p1+p2)/2
+
+print("A média foi de %.2f"%media)
+print("APROVADO" if media >= 6 else "REPROVADO")
