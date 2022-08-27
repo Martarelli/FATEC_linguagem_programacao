a0f8607affdc8f446a3181f4e7ee764e2d77f6d3
Martarelli <renanmartarelli@gmail.com>
2022-08-27 10:46:25 -0300

    inicio exercicio 8

---
+++ Class Room/Lista 1 - Funções/Exercicio 8.py
@@ -0,0 +1,1 @@
+# 8. (Função sem retorno sem parâmetro) Faça uma função/método que leia uma hora de início e de término de um jogo, ambas divididas em dois valores distintos: hora e minuto. Deverá ser apresentado a duração expressa em minutos, considerando que o tempo máximo de duração de um jogo é de 24 horas e que ele pode começar em um dia e terminar no outro.
