e247966c2e07a43ff7131103757230233da4b1cb
Martarelli <renanmartarelli@gmail.com>
2022-08-26 22:12:49 -0300

    refatoração do codigo

--- Class Room/Lista 1 - Funções/Exercicio 6.py
+++ Class Room/Lista 1 - Funções/Exercicio 6.py
@@ -3,14 +3,10 @@ def tabela():
     print("Olá, tudo bem?");
     x = int(input("Digite um número inteiro: "));
 
-    for i in range(x+1):
-        for j in range(i+1):
-            if j > 0:
-                print(i*j, end="\t");
-            else:
-                j +=1;
-        i += 1; 
-        print("\n");
+    for i in range(1,x+1):
+        for j in range(1,i+1):
+            print(i*j, end="\t");
+        print();
 
 def main():
     tabela();
