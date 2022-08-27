f515c8acf70acd456d60787cf4a76072e2357bb4
Martarelli <renanmartarelli@gmail.com>
2022-08-26 21:29:26 -0300

    inicio exercicio 5

---
+++ Class Room/Lista 1 - Funções/Exercicio 5.py
@@ -0,0 +1,10 @@
+# 5. (Função sem retorno sem parâmetro) Faça uma função/método que receba o preço antigo e atual de um produto, determine o percentual de acréscimo entre esses valores e apresente-o.
+def percentual():
+    print("Olá, tudo bem?");
+    numero = int(input("Digite um número para ser analisado: "));
+
+
+def main():
+    percentual();
+
+main();
