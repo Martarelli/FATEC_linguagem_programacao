a26f10513c0989cb22babf05de12aef594c5a59c
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-08-31 15:35:19 -0300

    exercicio 8 finalizado

--- Class Room/Lista 1 - Funções/Exercicio 8.py
+++ Class Room/Lista 1 - Funções/Exercicio 8.py
@@ -2,9 +2,35 @@
 
 def duracao_jogo():
     
-    hora_inicio = int(input("Informe a hora de inicio do jogo: "))
-    minuto_inicio = int(input("Informe os minutos de inicio do jogo: "))
-    hora_termino = int(input("Informe a hora de termino do jogo: "))
-    minuto_termino = int(input("Informe os minutos do termino do jogo: "))
+    hora_inicio = int(input("Informe a hora de inicio do jogo: "));
+    minuto_inicio = int(input("Informe os minutos de inicio do jogo: "));
+    hora_termino = int(input("Informe a hora de termino do jogo: "));
+    minuto_termino = int(input("Informe os minutos do termino do jogo: "));
 
+    if hora_inicio <= hora_termino and minuto_inicio <= minuto_termino:
+        hora_min = (hora_termino - hora_inicio) * 60;
+        minutos = minuto_termino - minuto_inicio;
+        duracao = hora_min + minutos;
+        print("A duração do jogo foi de %i minutos"%duracao);
+    elif hora_inicio <= hora_termino and minuto_inicio >= minuto_termino:
+        hora_min = (hora_termino - hora_inicio) * 60;
+        if hora_min == 0:
+            hora_min = 23 * 60;
+        minutos = minuto_inicio - minuto_termino;
+        duracao = hora_min + minutos;
+        print("A duração do jogo foi de %i minutos"%duracao);
+    elif hora_inicio > hora_termino and minuto_inicio <= minuto_termino:
+        hora_min = (hora_termino + 24 - hora_inicio) * 60;
+        minutos = minuto_termino - minuto_inicio;
+        duracao = hora_min + minutos; 
+        print("A duração do jogo foi de %i minutos"%duracao);
+    elif hora_inicio > hora_termino and minuto_inicio >= minuto_termino:
+        hora_min = (hora_termino + 24 - hora_inicio) * 60;
+        minutos = minuto_inicio - minuto_termino;
+        duracao = hora_min + minutos;
+        print("A duração do jogo foi de %i minutos"%duracao);
+        
+def main():
+    duracao_jogo();
     
+main();
