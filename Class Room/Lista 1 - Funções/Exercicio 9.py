696ed6f5cedf116e4e9b0d6375778ee8fbd829f1
Martarelli <renanmartarelli@gmail.com>
2022-09-02 19:16:21 -0300

    inicio exercicio 9

---
+++ Class Room/Lista 1 - Funções/Exercicio 9.py
@@ -0,0 +1,2 @@
+# 9. (Função sem retorno sem parâmetro) Faça uma função/método que leia cinco valores inteiros, determine e mostre o maior e o menor deles. Não pode usar vetor e função pronta da linguagem Python.
+
