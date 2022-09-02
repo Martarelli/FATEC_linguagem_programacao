b81ace849530feaeb1d5b37361a3e572b2362813
Martarelli <renanmartarelli@gmail.com>
2022-09-02 19:38:56 -0300

    inicio ex 10

---
+++ Class Room/Lista 1 - Funções/Exercicio 10.py
@@ -0,0 +1,3 @@
+# 10. (Função sem retorno sem parâmetro) Faça uma função/método que leia um valor inteiro e positivo N e mostre o valor de S, obtido pelo seguinte cálculo:
+# S = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N!
+# Observvação: Não pode usar vetor e função pronta da linguagem Python.
