757672413cf8f14382d01f8a4ef1c600539ada55
Martarelli <renanmartarelli@gmail.com>
2022-09-09 19:38:43 -0300

    EX 2 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 2.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 2.py
@@ -1,1 +1,14 @@
 # 2. (Função sem retorno com parâmetro) Faça um programa contendo uma função/método, sem parâmetro, que leia um nome e diga 'Olá nome, tudo bem?'. Crie outra função com o parâmetro nome, para que a mesma receba da main o nome digitado e apresente a mesma frase, este é um exemplo de PASSAGEM DE PARÂMETRO POR VALOR.
+def nome(s):
+    print("Olá %s, tudo bem? - COM PARAMETRO"%s);
+
+def print_nome():
+    name = input("Digite seu nome: ");
+    print("Olá %s, tudo bem? - SEM PARAMETRO"%name);
+
+def main():
+    print_nome();
+    name = input("Digite seu nome: ");
+    nome(name);
+
+main();
--- Class Room/Lista 2 - Funções c param s retorno/Exercicio1.py
+++
@@ -1,9 +0,0 @@
-# 1. (Função sem retorno com parâmetro) Faça um programa contendo uma função/método que leia um número e o multiplique por 2, apresente o resultado. Este é um exemplo de PASSAGEM DE PARÂMETRO POR VALOR.
-def mult(x):
-    print("O número %i X 2 é: "%x,(x * 2));
-
-def main():
-    n = int(input("Digite um número: "))
-    mult(n);
-
-main();
