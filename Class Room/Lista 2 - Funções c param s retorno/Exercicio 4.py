241c7fac4ea24186fcac64dd22640c7ee54ef010
Martarelli <renanmartarelli@gmail.com>
2022-09-09 19:46:50 -0300

    inicio ex 4

---
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 4.py
@@ -0,0 +1,2 @@
+# 4. (Função sem retorno com parâmetro) Faça uma função/método para a partir de um preço de um produto informado, calcular e apresentar o novo preço reajustado em 9%. Aqui deverá ocorrer, PASSAGEM DE PARÂMETRO POR VALOR.
+
