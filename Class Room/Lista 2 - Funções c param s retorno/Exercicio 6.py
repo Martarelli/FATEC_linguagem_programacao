e074517386e2291412a7b7ab68e334e25084f42a
Martarelli <renanmartarelli@gmail.com>
2022-09-09 20:15:48 -0300

    exercicio 6 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 6.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 6.py
@@ -1,1 +1,17 @@
 # 6. (Função sem retorno com parâmetro) Faça uma função/método para a partir de um valor inicial e um valor final realizar o acumulo desse valores e apresentar o resultado. Não use vetor. Aqui deverá ocorrer para as duas variáveis, PASSAGEM DE PARÂMETRO POR VALOR.
+def acumulo(x, y):
+    soma = 0;
+    for i in range(x, y+1):
+        soma += i;
+        
+    print("A soma dos número de %i a %i é %i" % (x, y, soma));
+
+
+def main():
+    x = int(input("Digite o primeiro número: "));
+    y = int(input("Digite o segundo número: "));
+
+    acumulo(x, y);
+
+
+main();
