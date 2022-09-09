252d6ec04e28f3b76896447624f8d82f840e73dd
Martarelli <renanmartarelli@gmail.com>
2022-09-09 19:56:09 -0300

    exercicio 4 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 4.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 4.py
@@ -1,2 +1,9 @@
 # 4. (Função sem retorno com parâmetro) Faça uma função/método para a partir de um preço de um produto informado, calcular e apresentar o novo preço reajustado em 9%. Aqui deverá ocorrer, PASSAGEM DE PARÂMETRO POR VALOR.
+def reajuste(x):
+    print("O novo preço (Reajuste 9%) é", x * 1.09);
+    
+def main():
+    x = int(input("Digite o valor do produto: "));
+    reajuste(x);
 
+main();
