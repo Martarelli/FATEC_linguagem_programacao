fef78804d8b7a1b2914824203c1e9139adf9de34
Martarelli <renanmartarelli@gmail.com>
2022-09-09 21:25:26 -0300

    exercicio 12 iniciado

---
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 12.py
@@ -0,0 +1,1 @@
+# 12. Faça uma função/método para verificar uma senha, contando/apresentando quantos dígitos numéricos e quantas letras existem. Aqui deverá ocorrer, PASSAGEM DE PARÂMETRO POR VALOR.
