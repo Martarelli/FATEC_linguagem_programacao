13dfc3da26885225c6c3fc0f83ab27ffc2311e4e
Martarelli <renanmartarelli@gmail.com>
2022-09-09 21:06:07 -0300

    exercicio 9 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 9.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 9.py
@@ -1,1 +1,15 @@
 # 9. (Função sem retorno com parâmetro) Faça uma função/método sem parâmetro, para ler um valor e chamar/criar OUTRA função (com parâmetro) que mostre se o valor é par ou ímpar.
+def par_ou_impar(p1):
+    if p1 % 2 == 0:
+        print("É Par");
+    else: 
+        print("É Impar")
+        
+def valor():
+    p1 = int(input("Digite um número: "));
+    par_ou_impar(p1);
+    
+def main():
+    valor();
+    
+main();
