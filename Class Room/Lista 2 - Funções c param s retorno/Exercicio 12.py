0725246afc2da8564094ae2d9dc35b60cf435453
Martarelli <renanmartarelli@gmail.com>
2022-09-09 21:28:24 -0300

    exercicio 12 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 12.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 12.py
@@ -1,1 +1,14 @@
 # 12. Faça uma função/método para verificar uma senha, contando/apresentando quantos dígitos numéricos e quantas letras existem. Aqui deverá ocorrer, PASSAGEM DE PARÂMETRO POR VALOR.
+def conta_numeros(s):
+    numeros = 0;
+    for i in range(len(s)):
+        if s[i].isdigit():
+            numeros += 1;
+    print("A senha tem %i números..."%numeros);
+    
+    
+def main():
+    s = input("Digite uma senha: ");
+    conta_numeros(s);
+    
+main();
