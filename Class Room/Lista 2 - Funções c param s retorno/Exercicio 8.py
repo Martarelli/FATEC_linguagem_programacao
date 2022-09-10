b306cc7f4f7ca93e5cf1da85c6cc8ddff98be7ea
Martarelli <renanmartarelli@gmail.com>
2022-09-09 21:02:27 -0300

    exercicio 8 finalizado

--- Class Room/Lista 2 - Funções c param s retorno/Exercicio 8.py
+++ Class Room/Lista 2 - Funções c param s retorno/Exercicio 8.py
@@ -1,1 +1,18 @@
 # 8. (Função sem retorno com parâmetro) Faça uma função/método que calcule a média de um aluno que realizou duas provas (P1 e P2), a partir desta média, chame/crie OUTRA função que verifique se esta média aprova ou reprova o aluno. Aqui deverá ocorrer nas duas funções, PASSAGEM DE PARÂMETRO POR VALOR.
+def media(p1, p2):
+    x = (p1 + p2)/2;
+    aprovado(x);
+    
+def aprovado(media):
+    if media >= 6:
+        print("APROVADOOOOOO");
+    else:
+        print("REPROVADOOOO :C");
+        
+def main():
+    p1 = int(input("Digite a nota p1: "));
+    p2 = int(input("Digite a nota p2: "));
+    media(p1, p2);
+
+
+main();
