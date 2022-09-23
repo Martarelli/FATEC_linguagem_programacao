a573cb5cfa5a5796fcdcb798738ca08e75d26333
Martarelli <renanmartarelli@gmail.com>
2022-09-23 19:49:15 -0300

    exercicio 2 finalizado

---
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 2.py
@@ -0,0 +1,9 @@
+def somar(n1, n2):
+    return n1 + n2
+
+def main():
+    num1 = int(input("Digite o primeiro valor: "))
+    num2 = int(input("Digite o segundo valor: "))
+    print("O resultado é:", somar(num1, num2))
+    
+main()
