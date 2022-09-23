597ccc7aa7d3d4ac86666a695460cc33fddc7ccb
Martarelli <renanmartarelli@gmail.com>
2022-09-23 20:31:15 -0300

    exercicio 3 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 3.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 3.py
@@ -1,1 +1,12 @@
 # 3. (Função com retorno sem parâmetro) Faça um programa contendo uma função/método que leia a base e a altura de um triângulo e retorne/apresente sua área A = (base x altura)/2.
+def area():
+    b = int(input("Qual tamanho da base do triangulo?: "))
+    h = int(input("Qual altura do triangulo?: "))
+    area = (b * h)/2
+    return area
+
+def main():
+    a = area()
+    print("A área do triangulo é: %.2f"%a)
+    
+main()
