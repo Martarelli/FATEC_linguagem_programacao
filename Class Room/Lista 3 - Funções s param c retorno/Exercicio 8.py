9893652530cd17abb720fd70bab261ac3be9721c
Martarelli <renanmartarelli@gmail.com>
2022-09-23 19:42:30 -0300

    exercicio 8 finalizado

---
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 8.py
@@ -0,0 +1,5 @@
+# 8. (Função com retorno sem parâmetro) Faça uma função/método retorne um vetor com os três primeiros números perfeitos. Sabe-se que um número é perfeito quando é igual a soma de seus divisores (exceto ele mesmo).
+# Exemplo: os divisores de 6 são 1, 2 e 3, e 1 + 2 + 3 = 6, logo 6 é perfeito. Não use função pronta.
+# 1º número perfeito: 6
+# 2º número perfeito: 28
+# 3º número perfeito: 496
