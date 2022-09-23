ae3ce9b638dd9e10a1791e55ece867c322ee448e
Martarelli <renanmartarelli@gmail.com>
2022-09-23 19:44:15 -0300

    exercicio 9 iniciada

---
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 9.py
@@ -0,0 +1,21 @@
+# 9. (Função com retorno sem parâmetro) Foi realizada uma pesquisa sobre algumas características físicas de cinco habitantes de uma região. Foram coletados os seguintes dados de cada habitante: idade, sexo (M - masculino ou F - feminino), cor dos olhos (A - azuis ou C - castanhos), cor dos cabelos (L - louros, P - pretos ou C - castanhos).
+
+#    1. Faça uma função/método que leia esses dados, armazenando-os em vetores (listas);
+#    2. Faça uma função/método que determine e devolva a função principal a média de idades das pessoas com olhos castanhos e cabelos pretos;
+#    3. Faça uma função/método que determine e devolva a função principal a maior idade entre os habitantes;
+#    4. Faça uma função/método que determine e devolva a função principal a quantidade de indivíduos do sexo feminino com idade entre 18 e 35 anos(inclusive) e que tenham olhos azuis e cabelos louros.
+
+# Declarar os vetores antes de qualquer programação, indica que 
+# que estas variáveis serão globais a todo o restante do programa 
+# e das funções 
+idade = []
+sexo = []
+olho = []
+cabelo = []
+
+def cadastrar():
+...
+...
+def main():
+
+main()
