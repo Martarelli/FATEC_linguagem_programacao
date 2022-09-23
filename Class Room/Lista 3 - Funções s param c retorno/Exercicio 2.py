b5ffbe81c654c66725e512b68434681933552f30
Martarelli <renanmartarelli@gmail.com>
2022-09-23 19:57:05 -0300

    exercicio 2 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 2.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 2.py
@@ -1,2 +1,10 @@
 # 2. (Função com retorno sem parâmetro) Faça um programa contendo uma função/método para subtrair dois números, retornar o resultado e o apresentando.
+def subtracao():
+    n1 = int(input("Digite o primeiro número: "))
+    n2 = int(input("Digite o segundo número: "))
+    print("O resultado é:",n1-n2)
 
+def main():
+    subtracao()
+    
+main()
