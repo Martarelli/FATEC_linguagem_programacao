685214293f90f275634e6f9e6bad175c340274ef
Martarelli <renanmartarelli@gmail.com>
2022-09-23 20:37:49 -0300

    exercicio 5 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 5.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 5.py
@@ -1,1 +1,12 @@
 # 5. (Função com retorno sem parâmetro) Faça um programa contendo uma função/método que verifique se um número é par, retorne mostrando a str/string ‘É par’ ou se ‘É ímpar’.
+def verifica():
+    x = int(input("Digite um número: "))
+    if x % 2 == 0:
+        return "é Par"
+    else:
+        return "é Impar"
+def main():
+    verificacao = verifica()
+    print("O número digitado",verificacao)
+    
+main()
