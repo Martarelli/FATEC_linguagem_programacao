49db74d48641a710c456ad8aedcecf4a8e4bf53d
Martarelli <renanmartarelli@gmail.com>
2022-09-23 20:59:10 -0300

    exercicio 7 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 7.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 7.py
@@ -1,1 +1,19 @@
 # 7. (Função com retorno sem parâmetro) Faça uma função/método que leia e armazene 5 elementos inteiros no vetor A, deverá ser gerado um vetor B, de mesmo tamanho, que armazenará o fatorial de cada elemento de A. Não use função pronta de cálculo de fatorial. Retorne/apresente o vetor B.
+
+def fatorial():
+    A = []
+    B = []
+    for i in range(5):
+        A.append(int(input("Digite um número: ")))
+
+    for i in A:
+        fact = 1
+        for j in range(1,i + 1):
+            fact = fact * j
+        B.append(fact)
+    return B
+
+def main():
+    x = fatorial()
+    print(x)
+main()
