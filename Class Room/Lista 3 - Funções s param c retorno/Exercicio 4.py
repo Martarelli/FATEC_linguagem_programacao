e000bc91606780d97ddeec09d564bf83cf1bf565
Martarelli <renanmartarelli@gmail.com>
2022-09-23 20:33:56 -0300

    exercicio 4 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 4.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 4.py
@@ -1,1 +1,11 @@
 # 4. (Função com retorno sem parâmetro) Faça um programa contendo uma função/método que leia o lado de um quadrado e retorne sua área, area = lado².
+
+def area():
+    l = int(input("Qual tamanho do lado do quadrado?: "))
+    area = l ** 2
+    return area
+
+def main():
+    a = area()
+    print("A area do quadrado é  %.2f"%a)    
+main()
