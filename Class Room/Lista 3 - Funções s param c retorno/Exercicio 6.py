fcc4869593c22a7dbe520626b10cff75f9c94311
Martarelli <renanmartarelli@gmail.com>
2022-09-23 20:39:57 -0300

    exercicio 6 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 6.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 6.py
@@ -1,1 +1,13 @@
 # 6. (Função com retorno sem parâmetro) Faça um programa contendo uma função/método que verifique se um número é par, retorne/mostre o valor bool True para par e False para ímpar.
+
+def verifica():
+    x = int(input("Digite um número: "))
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+    
+def main():
+    print(verifica())
+    
+main()
