24f4700f8854fbe9f258e9483f65206fd620ef47
Martarelli <renanmartarelli@gmail.com>
2022-09-23 22:03:37 -0300

    exercicio 9 finalizado

--- Class Room/Lista 3 - Funções s param c retorno/Exercicio 9.py
+++ Class Room/Lista 3 - Funções s param c retorno/Exercicio 9.py
@@ -8,14 +8,47 @@
 # Declarar os vetores antes de qualquer programação, indica que 
 # que estas variáveis serão globais a todo o restante do programa 
 # e das funções 
-idade = []
-sexo = []
-olho = []
-cabelo = []
+
 
 def cadastrar():
-...
-...
+    idade = []
+    sexo = []
+    olho = []
+    cabelo = []
+    
+    for i in range(5):
+        idade.append(int(input("Digite a idade: ")))
+        sexo.append(input("sexo (M - masculino ou F - feminino)"))
+        olho.append(input("cor dos olhos (A - azuis ou C - castanhos)"))
+        cabelo.append(input("cor dos cabelos (L - louros, P - pretos ou C - castanhos)"))
+        
+    media_cast_preto = 0
+    soma_idade = 0
+    qtd_cast_preto = 0
+    for i in range(5):
+        if olho[i].upper() == "C" and cabelo[i].upper() == "P":
+            soma_idade += idade[i]
+            qtd_cast_preto += 1
+    
+    media_cast_preto = soma_idade / qtd_cast_preto
+    
+    maior_idade = 0
+    for i in range(5):
+        if idade[i] > maior_idade:
+            maior_idade = idade[i]
+    
+    ind_feminino = 0
+    for i in range(5):
+        if 18 <= idade[i] >= 35 and olho[i].upper() == "A" and cabelo[i].upper() == "L":
+            ind_feminino += 1
+            
+    return media_cast_preto, maior_idade, ind_feminino            
+    
 def main():
-
+    media, idade_max, fem = cadastrar()
+    print("A média de idades das pessoas com olhos castanhos e cabelos pretos:",media)
+    print("A maior idade entre os habitantes", idade_max) 
+    print("A quantidade de indivíduos do sexo feminino com idade entre 18 e 35 anos(inclusive) e que tenham olhos azuis e cabelos louros.", fem) 
+    
+    
 main()
