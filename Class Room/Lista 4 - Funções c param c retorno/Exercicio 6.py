feefcbf05a0b0d8a70044dc426b1c1b30abc0daa
Martarelli <renanmartarelli@gmail.com>
2022-09-28 20:51:41 -0300

    alteração exercicio 6

--- Class Room/Lista 4 - Funções c param c retorno/Exercicio 6.py
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 6.py
@@ -14,3 +14,25 @@
 # Observação: Na chamada da função deve-se utilizar uma estrutura de repetição que permita diversos cálculos, quando o usuário quiser sair da calculadora digitará qualquer valor diferente dos números do menu.
 
 # A função da construção do menu, chamará todas as outras passando a elas o valor digitado.
+
+def quadrado(n1):
+    return n1 * n1
+
+def cubo(n1):
+    return n1 * n1 * n1
+
+def raiz(n1):
+    return pow(n1, 2)
+
+def calculadora(opt):
+    n1 = int(input("Digite um número: "))
+    if opt == 1:
+        print(raiz(n1))
+        
+    
+def main():
+    print("Menu de cálculos\n1.Número ao quadrado\n2.Número ao cubo\n3.Raiz do número\n4.Raiz cúbica do número\nQual é a opção desejada?")
+    opt = int(input())
+    calculadora(opt)
+    
+main()
