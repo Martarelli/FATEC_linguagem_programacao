a8cd677ef490cc3387f8e107b1fd52f0a4fee352
Martarelli <renanmartarelli@gmail.com>
2022-09-28 19:51:35 -0300

    exercicio 3 iniciado

---
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 3.py
@@ -0,0 +1,7 @@
+## 3. (Função com retorno com parâmetro) Faça um programa contendo uma função/método que receba um valor de porcentagem de aumento via parâmetro, aplique este aumento a um salário de um funcionário, retorne e apresente o novo salário.
+
+def main():
+    aumento = int(input("Qual a % de aumento?: "))
+    
+    
+main()
