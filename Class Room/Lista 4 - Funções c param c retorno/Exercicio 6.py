0caba1d4a6033575dfd1aad4ae22b7f299b5eaa0
Martarelli <renanmartarelli@gmail.com>
2022-09-28 20:27:59 -0300

    exercicio 6 iniciado

---
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 6.py
@@ -0,0 +1,16 @@
+# 6. (Função com retorno com parâmetro) Faça um programa contendo algumas funções:
+
+# a) Função para construir um menu de opções para uma calculadora, como neste exemplo:
+
+# Menu de cálculos
+# 1.   Número ao quadrado
+# 2.   Número ao cubo
+# 3.   Raiz do número
+# 4.   Raiz cúbica do número
+# Qual é a opção desejada?
+
+# b) Desenvolva uma função para cada opção de cálculo.
+
+# Observação: Na chamada da função deve-se utilizar uma estrutura de repetição que permita diversos cálculos, quando o usuário quiser sair da calculadora digitará qualquer valor diferente dos números do menu.
+
+# A função da construção do menu, chamará todas as outras passando a elas o valor digitado.
