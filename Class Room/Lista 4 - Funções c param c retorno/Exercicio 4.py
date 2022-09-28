93992fbb806973dee266fc725278e5e3d6c2c59c
Martarelli <renanmartarelli@gmail.com>
2022-09-28 20:03:52 -0300

    exercicio 4 iniciado

---
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 4.py
@@ -0,0 +1,1 @@
+# 4. (Função com retorno com parâmetro) Faça um programa contendo uma função/método que receba duas notas (P1, P2) calcule a média, chame outra função na main que avalie se este aluno esta aprovado ou reprovado retornando a str/string 'Aprovado' ou 'Reprovado'.
