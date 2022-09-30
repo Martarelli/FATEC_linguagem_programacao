c82d1521668da0d6bc298add3f10ab85aeb1bdfc
Martarelli <renanmartarelli@gmail.com>
2022-09-30 19:48:41 -0300

    exercicio finalizado

--- Class Room/Lista 4 - Funções c param c retorno/Exercicio 8.py
+++ Class Room/Lista 4 - Funções c param c retorno/Exercicio 8.py
@@ -16,3 +16,38 @@
 # Observação: Na chamada da função deve-se utilizar uma estrutura de repetição que permita diversos cálculos, quando o usuário quiser sair da calculadora digitará qualquer valor diferente dos caracteres do menu.
 
 # A função de desenho/construção do menu, chamará todas as outras passando a elas os valores digitados.
+
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):
+    return n1 - n2
+
+def divisao(n1, n2):
+    return n1 / n2
+
+def multiplicacao(n1, n2):
+    return n1 * n2
+
+def main():
+    ligado = True
+    while ligado:
+        print("\t*** Minha Calculadora ***")
+        n1 = int(input("Digite um número.....: ")) 
+        n2 = int(input("Digite outro número..: ")) 
+        print("+ Soma dois números\n- Subtrai dois números\n* Multiplica dois números\n/ Divide dois números\n")
+        op = input("Qual opção deseja?: ")
+        
+        if op == "+":
+            print("\nResultado de %i + %i = %i\n"%(n1, n2, soma(n1, n2)))
+        elif op == "-":
+            print("\nResultado de %i - %i = %i\n"%(n1, n2, subtracao(n1, n2)))
+        elif op == "/":
+            print("\nResultado de %i / %i = %.2f\n"%(n1, n2, divisao(n1, n2)))
+        elif op == "*":
+            print("\nResultado de %i * %i = %i\n"%(n1, n2, multiplicacao(n1, n2)))
+        else:
+            print("Desligando a calculadora.....")
+            ligado = False
+
+main()
