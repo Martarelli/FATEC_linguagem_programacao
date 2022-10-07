ead9f0ddf94097f9492c0a46da9f92a83d36d895
Martarelli <renanmartarelli@gmail.com>
2022-10-07 19:29:41 -0300

    exercicio 3 iniciado

---
+++ 1° Bimestre/Class Room/Prova/Exercicio 3.py
@@ -0,0 +1,15 @@
+# 3. [2,50 pontos] O IPVA (Imposto sobre a Propriedade de Veículos Automotores) no estado de São Paulo, possui valor de alíquota de imposto variável de acordo com o tipo de veículo automotor.
+# Crie uma função que receba como parâmetro o valor do veículo e uma letra, F, ou E, ou C, ou M, ou O, ou A, a função retornará a porcentagem para auxiliar o cálculo do item b). Para esta verificação, siga a instrução do governo para cada tipo de veículo automotor: Flex => 4%, Elétrico => 3%, Caminhonete => 2%, Moto => 2%, Onibus => 2%, cAminhao 1.5%.
+# Dependendo do ano de fabricação, pode ocorrer isenção de IPVA, ou seja, não é pago nada, referente a este imposto. O estado de São Paulo concede isenção a partir de 20 anos de fabricação.
+# Atendendo a 10 proprietários de automóveis, a partir dos anos de fabricação, tipo de veículo automotor e dos valores dos carros, crie um programa que atenda cada item a seguir, na main() calcule e apresente:
+
+# a) Quantidade de veículos automotores.
+
+# b) Valor do IPVA de veículo automotor, use a função requerida.
+
+# c) Média de valores dos veículos automotores.
+
+# d) Total dos IPVAs pagos.
+# Não use vetor/matriz.
+
+# Curiosidade: O valor do veículo geralmente é avaliado pela tabela FIPE (Fundação Instituto de Pesquisas Econômicas).
