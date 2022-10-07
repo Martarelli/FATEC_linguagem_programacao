fe18b91759386bc63c752c6be69fb337c46a4718
Martarelli <renanmartarelli@gmail.com>
2022-10-07 19:28:46 -0300

    inicio exercicio 1

---
+++ 1° Bimestre/Class Room/Prova/Exercicio 1.py
@@ -0,0 +1,8 @@
+# 1. [2,00 pontos] O recebimento de produtos comprados por um hipermercado, é distribuído de forma igualitaria, 8 caixas de produtos em cada palete, para serem armazenados no depósito. O resto das caixas são colocadas nas gondolas.
+# Crie uma função/método (sem parâmetro) utilizando a linguagem Python, que calcule e apresente a quantidade de paletes necessários. Retorne o resto da quantidade de caixas.
+# Implemente outra função/método (com parâmetro) que calcule e apresente, do resto das caixas a quantidade de produtos que serão colocados nas gondolas. Cada caixa tem 12 produtos.
+# Utilize a função main() para a chamada das funções.
+
+# Lembrete, na função que tem parâmetro, este, neste caso, na programação, é chamado de PASSAGEM DE PARÂMETRO POR VALOR.
+
+# Curiosidade: Palete é um estrado de madeira, metal ou plástico que é utilizado para empilhar ou transportar materiais por meio de empilhadeiras.
