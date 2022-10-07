27071701257e01d7f18ee5380438d53ed8455ece
Martarelli <renanmartarelli@gmail.com>
2022-10-07 19:29:13 -0300

    exercicio 2 iniciado

---
+++ 1° Bimestre/Class Room/Prova/Exercicio 2.py
@@ -0,0 +1,9 @@
+# 2. [1,50 ponto] Faça uma função que receba como parâmetro uma frase, conte e apresente:
+
+# a) quantas vogais;
+
+# b) quantas consoantes;
+
+# c) e quantos números tem na frase.
+
+# Lembrete, quando um parâmetro é um vetor, este na programação, é chamado de PASSAGEM DE PARÂMETRO POR REFERÊNCIA.
