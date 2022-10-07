0ff5aad529703a989941a2dd9e38556e68ac7edc
Martarelli <renanmartarelli@gmail.com>
2022-10-07 19:40:15 -0300

    exercicio 1 finalizado

--- 1° Bimestre/Class Room/Prova/Exercicio 1.py
+++ 1° Bimestre/Class Room/Prova/Exercicio 1.py
@@ -1,5 +1,7 @@
 # 1. [2,00 pontos] O recebimento de produtos comprados por um hipermercado, é distribuído de forma igualitaria, 8 caixas de produtos em cada palete, para serem armazenados no depósito. O resto das caixas são colocadas nas gondolas.
+
 # Crie uma função/método (sem parâmetro) utilizando a linguagem Python, que calcule e apresente a quantidade de paletes necessários. Retorne o resto da quantidade de caixas.
+
 # Implemente outra função/método (com parâmetro) que calcule e apresente, do resto das caixas a quantidade de produtos que serão colocados nas gondolas. Cada caixa tem 12 produtos.
 # Utilize a função main() para a chamada das funções.
 
@@ -10,13 +12,16 @@ def paletes():
     qtd_recebida = int(input("Qual a quantidade de produtos recebidos?: "))
     qtd_paletes = qtd_recebida // 8
     resto = qtd_recebida - (qtd_paletes * 8)
-    print("Quantidade de paletes necessários: %i - Resto de produtos %i"%(qtd_paletes, resto))
+    print("Quantidade de paletes necessários: %i - Caixas restantes %i"%(qtd_paletes, resto))
     
     return resto
+
+def produtos_gondula(cx):
+    qtd_produtos = cx * 12
+    print("Serão colocados %i produtos nas gondulas"%qtd_produtos)
     
 def main():
     resto_paletes = paletes()
-    print(resto_paletes)
-    
+    produtos_gondula(resto_paletes)    
     
 main()
