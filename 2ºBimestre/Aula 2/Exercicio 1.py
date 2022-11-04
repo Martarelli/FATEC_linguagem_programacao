690f6df57168bf1f73e7153b2385b2947fe7ae54
Martarelli <renanmartarelli@gmail.com>
2022-11-04 19:32:47 -0300

    aviso de termino de execução

--- 2ºBimestre/Aula 2/Exercicio 1.py
+++ 2ºBimestre/Aula 2/Exercicio 1.py
@@ -2,3 +2,4 @@ arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a')
 for linha in range(1,5):
     arquivo.write("Linha número com append %i\n" %linha)
 arquivo.close()
+print("Termino de execução....")
