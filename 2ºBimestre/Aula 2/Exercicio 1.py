cc09bd849d941e52d236be86ecd3412d8f8ce52d
Martarelli <renanmartarelli@gmail.com>
2022-11-04 19:31:33 -0300

    exemplo uso de append

--- 2ºBimestre/Aula 2/Exercicio 1.py
+++ 2ºBimestre/Aula 2/Exercicio 1.py
@@ -1,4 +1,4 @@
-arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'w')
-for linha in range(1,21):
-    arquivo.write("Linha número %i\n" %linha)
+arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a')
+for linha in range(1,5):
+    arquivo.write("Linha número com append %i\n" %linha)
 arquivo.close()
