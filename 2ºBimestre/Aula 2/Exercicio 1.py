167979731c6a576816edc1a3998dc6f0a5a9a90a
Martarelli <renanmartarelli@gmail.com>
2022-11-04 19:37:49 -0300

    adicionado exemplo dos dois modos

--- 2ºBimestre/Aula 2/Exercicio 1.py
+++ 2ºBimestre/Aula 2/Exercicio 1.py
@@ -1,5 +1,7 @@
-arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a')
+arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'w')
+# arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a') adiciona ao final
 for linha in range(1,5):
     arquivo.write("Linha número com append %i\n" %linha)
+    # arquivo.write("Linha número com append %i\n" %linha)
 arquivo.close()
 print("Termino de execução....")
