3f943c1e1648c85600f44a7dae33d8ce3cea69c5
Martarelli <renanmartarelli@gmail.com>
2022-11-04 20:49:50 -0300

    exercicio 4 finalizado

--- 2ºBimestre/Classroom/Lista 1/Exercicio 4.py
+++ 2ºBimestre/Classroom/Lista 1/Exercicio 4.py
@@ -6,3 +6,66 @@
 #     Consulta por nome
 #     Visualizar todos os dados
 #     Sair
+
+class Alunos:
+    nome = ""
+    data_nasc = ""
+    telefone = ""
+    endereco = ""
+    serie_atual = ""
+    
+def cadastrar():
+    alu = Alunos()
+    alu.nome = input("NOME COMPLETO: ")
+    alu.data_nasc = input("DATA NASCIMENTO (dd/MM/yyyy): ")
+    alu.telefone = input("TELEFONE: ")
+    alu.endereco= input("ENDEREÇO: ")
+    alu.serie_atual= input("SERIE ATUAL: ")
+    
+    return alu
+
+def consultarNome(cad, nome):
+    print("\nProcurando aluno(a): %s"%nome)
+    encontrado = False
+    for i in cad:
+        if nome == i.nome:   
+            print("NOME: %s\nDATA DE NASCIMENTO: %s\nTELEFONE: %s\nENDEREÇO: %s\nSERIE ATUAL: %s"%(i.nome,i.data_nasc,i.telefone,i.endereco, i.serie_atual))
+            encontrado = True
+            
+    if encontrado == False:
+        print("Aluno não encontrado........\n")
+
+        
+def exibirTodos(cad):
+    for i in cad:
+        print("\n-----------------\n")
+        print("NOME: %s\nDATA DE NASCIMENTO: %s\nTELEFONE: %s\nENDEREÇO: %s\nSERIE ATUAL: %s"%(i.nome,i.data_nasc,i.telefone,i.endereco, i.serie_atual))
+        
+def main():
+    op = 1
+    alunos = []
+    while op != 0:
+        print("----- Menu Principal -----\n")
+        print("1 - Cadastrar Aluno")
+        print("2 - Consulta por nome")
+        print("3 - Visualizar todos os dados")
+        print("4 - Sair")
+    
+        op2 = int(input("Digite a opção desejada: "))
+        
+        if op2 == 1:
+            novo_cadastro = cadastrar()
+            alunos.append(novo_cadastro)
+        elif op2 == 2:
+            nome_procurado = input("Digite o NOME COMPLETO que deseja procurar: ")
+            consultarNome(alunos, nome_procurado)
+        elif op2 == 3:
+            exibirTodos(alunos)
+        elif op2 == 4:
+            print("Finalizando o programa....")
+            op = 0
+        else: 
+            print("Opção inválida.. tente novamente...")
+
+    
+main()
