0a519c52f35394a29df534d02d4d8fdd70a9885f
Martarelli <renanmartarelli@gmail.com>
2022-11-04 20:30:06 -0300

    exercicio 4 iniciado

---
+++ 2ºBimestre/Classroom/Lista 1/Exercicio 4.py
@@ -0,0 +1,8 @@
+# 4. Uma escola precisa montar o cadastro geral de seus alunos. Este cadastro deverá conter as seguintes informações por aluno: nome completo, data de nascimento, telefone, endereço e série atual. Levando em conta que esta escola possui no máximo 500 alunos, como você faria para estruturar estas informações num sistema de gerenciamento para a escola? Implemente utilizando estrutura. Também use a criação de funções para cada operação. Use o menu a seguir:
+
+# Menu de opções:
+
+#     Cadastrar alunos
+#     Consulta por nome
+#     Visualizar todos os dados
+#     Sair
