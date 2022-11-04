ee89c7a35c49a3bff901ee70203a700dedf6c251
Martarelli <renanmartarelli@gmail.com>
2022-11-04 19:41:04 -0300

    exemplo de leitura de arquivo

--- 2ºBimestre/Aula 2/Exercicio 1.py
+++ 2ºBimestre/Aula 2/Exercicio 1.py
@@ -1,7 +1,14 @@
-arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'w')
-# arquivo = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a') adiciona ao final
+arquivo_escrita = open('./2ºBimestre/Aula 2/exercicio1.txt', 'w')
+# arquivo_escrita = open('./2ºBimestre/Aula 2/exercicio1.txt', 'a') adiciona ao final
 for linha in range(1,5):
-    arquivo.write("Linha número %i\n" %linha)
-    # arquivo.write("Linha número com append %i\n" %linha)
-arquivo.close()
-print("Termino de execução....")
+    arquivo_escrita.write("Linha número %i\n" %linha)
+    # arquivo_escrita.write("Linha número com append %i\n" %linha)
+arquivo_escrita.close()
+print("Termino da criação do arquivo....")
+
+arquivo_leitura = open(f'./2ºBimestre/Aula 2/exercicio1.txt', 'r')
+for x in arquivo_leitura.readlines():
+    print(x)
+arquivo_leitura.close()
+print("Termino da leitura do arquivo....")
+
