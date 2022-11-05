7caf67696b994f5c488463230e2b7ea2746fafd2
Martarelli <renanmartarelli@gmail.com>
2022-11-04 21:44:44 -0300

    exercicio 6 iniciado

---
+++ 2ºBimestre/Classroom/Lista 1/Exercicio 6 .py
@@ -0,0 +1,7 @@
+# 6. Elabore uma estrutura para representar um Funcionario (código, nome, endereço, salário). Para o membro endereço deve-se criar outra estrutura Endereço (logradouro, número, bairro, cidade). Utilize aninhamento de estruturas para resolver este desenvolvimento. Construa uma função para cada opçao do menu a seguir:
+
+# Menu de opções:
+
+#     Cadastrar funcionários
+#     Visualizar todos os dados
+#     Sair
