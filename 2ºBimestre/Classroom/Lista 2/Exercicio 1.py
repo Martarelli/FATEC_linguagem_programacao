3a6755c365fc449e333cd73bad8544df5f7bf73b
Martarelli <renanmartarelli@gmail.com>
2022-11-11 21:47:37 -0300

    exercicio 1 iniciado

---
+++ 2ºBimestre/Classroom/Lista 2/Exercicio 1.py
@@ -0,0 +1,7 @@
+# 1. Elabore uma estrutura para representar e armazenar 10 fornecedores (código, nome, endereco). Utilize os recursos de arquivo para armazenar estes dados permanentemente. O nome do arquivo deve ser o mesmo da estrutura. Construa um menu com as seguintes opções, cada uma delas deve ter uma função e a main para chamar todas elas.
+
+# Menu de opções:
+
+#     Cadastrar fornecedores
+#     Visualizar todos os dados
+#     Sair
