ce0c547417d4ea9d91c996128d50a0e678c868cd
Martarelli <renanmartarelli@gmail.com>
2022-11-18 20:48:49 -0300

    exercicio 3 inicializado

---
+++ 2ºBimestre/Classroom/Lista 2/Exercicio 3.py
@@ -0,0 +1,8 @@
+# 3. Elabore uma estrutura para representar e armazenar 10 alunos (matricula, nome, telefone). Utilize os recursos de arquivo para armazenar estes dados permanentemente. O nome do arquivo deve ser o mesmo da estrutura. Construa um menu com as seguintes opções, cada uma delas deve ter uma função e a main para chamar todas elas.
+
+# Menu de opções:
+
+#     Cadastrar alunos
+#     Visualizar todos os dados
+#     Sair
+
