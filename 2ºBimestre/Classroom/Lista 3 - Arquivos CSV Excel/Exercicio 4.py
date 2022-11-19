c92bc9e1059913e484b2ab8396e592e98777bebf
Martarelli <renanmartarelli@gmail.com>
2022-11-18 21:41:17 -0300

    exercicio 4 finalizado

---
+++ 2ºBimestre/Classroom/Lista 3 - Arquivos CSV Excel/Exercicio 4.py
@@ -0,0 +1,4 @@
+# 4. Crie um programa que carregue o arquivo Propaganda.csv, por meio da biblioteca Pandas. Calcule e apresente o valor máximo da coluna do arquivo 'Jornal'.
+import pandas as pd
+df1 = pd.read_csv('Propaganda.csv')
+print("Valor máximo: R$%.2f"%df1["Jornal"].max())
