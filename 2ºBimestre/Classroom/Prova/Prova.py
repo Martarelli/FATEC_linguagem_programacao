bc8f7a86434b5f6dcaafc9b659ee0b0e953a6910
Martarelli <renanmartarelli@gmail.com>
2022-12-02 19:18:03 -0300

    instruções da prova

---
+++ 2ºBimestre/Classroom/Prova/Prova.py
@@ -0,0 +1,25 @@
+# 1. [6,0 pontos] Em Python crie um programa que atenda o menu a seguir. Desenvolva uma função para cada opção do menu. Utilize a criação de estruturas. Cadastrar 5 (vetor) compras e 2 (vetor) clientes.
+# CLIENTES 	- 	COMPRAS
+# cod_cli 	- 	cod_comp
+# nome 	- 	cod_cli
+# saldo_cashback 	- 	valor
+
+# Menu de Opções
+
+# 1 Cadastrar clientes
+# 2 Mostrar todos os clientes
+# 3 Cadastrar compras
+# 4 Mostrar todas as compras
+# 5 Mostrar o total de todas as compras realizadas por um cliente
+# 6 Armazenar todas as compras em arquivo
+# 7 Apresentar o conteúdo do arquivo
+# 8 Gerar um gráfico
+# 9 Sair
+
+# Observaçôes:
+
+#     Na opção 3, o valor da compra, deve ser descontado no saldo do cashback do cliente que a realizou. Por exemplo, cliente 1, tem 500 de saldo, se ele realizar uma compra de 100 reais, o saldo do cashback passará para 400.
+#     Na opção 5, o usuário do sistema informará/digitará o código do cliente que quer e o sistema/programa fará uma pesquisa e calculará o total de compras do determinado cliente.
+#     Na opção 6 use a função write antes da estrutura de repetição para armazenar o cabeçalho das colunas, depois dentro do laço grave todos os campos do tipo Compras como foi visto em aula, no arquivo de nome compras.csv. Use como sepador entre colunas/campos a , vírgula
+#     Na opção 7 apresente todos os dados armazenados no arquivo compras.csv.
+#     Na opção 8 use a coluna valor no gráfico e adote a cor orange da biblioteca Matplotlib
